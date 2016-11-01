bda1141766e48bd938beccee123bb3e60d47f8dc
Titus Malo <Titus Malo>
2016-11-01 15:32:50 +0300

    Initial commit

---
+++ app/src/main/java/com/mallonic/cdf/ApplyActivity.java
@@ -0,0 +1,15 @@
+package com.mallonic.cdf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Malo on 9/18/2016.
+ */
+public class ApplyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply);
+    }
+}
---
+++ app/src/main/java/com/mallonic/cdf/CheckActivity.java
@@ -0,0 +1,18 @@
+package com.mallonic.cdf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
